1586b171e29bea6cd2dc43fc6e1b1ec7aa0efe4e
chochoq <aubepe@gmail.com>
2021-08-29 03:02:07 +0900

    (add) local storage username

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const HIDDEN_CLASS_NAME = "hidden";
 function onLoginSubmit(e) {
     e.preventDefault();
     const userName = loginInput.value;
+    localStorage.setItem("userName", userName);
     loginForm.classList.add(HIDDEN_CLASS_NAME);
     greeting.innerText = `Welcome back ${userName}!!`;
     greeting.classList.remove(HIDDEN_CLASS_NAME);
