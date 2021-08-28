403363e55253238df3140129270b1ca0b6b8eaca
chochoq <aubepe@gmail.com>
2021-08-29 04:45:51 +0900

    (add) padStart with clock

--- js/clock.js
+++ js/clock.js
@@ -3,7 +3,11 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+    const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const second = String(date.getSeconds()).padStart(2, '0');
+    
+    clock.innerText = `${hour} : ${minute} : ${second}`;
 }
 
 getClock();
