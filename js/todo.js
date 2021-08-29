e6ce4cec2f86e52295eabdd73bb63968b7e318ac
chochoq <aubepe@gmail.com>
2021-08-29 20:07:39 +0900

    (add) delete todolist

--- js/todo.js
+++ js/todo.js
@@ -2,20 +2,30 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+function deleteTodoList(e) {
+    const li = e.target.parentElement;
+    li.remove();
+};
+
 function paintTodo(newTodo) {
     const li =document.createElement("li");
     const span = document.createElement("span");
+    const btn = document.createElement("button");
+
+    span.innerHTML = newTodo;
+    btn.innerHTML = "삭제";
+    btn.addEventListener("click",deleteTodoList);
 
     li.appendChild(span);
-    span.innerHTML = newTodo;
+    li.appendChild(btn);
     todoList.appendChild(li)
-}
+};
 
 function todoSubmit(e) {
     e.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
     paintTodo(newTodo);
-}
+};
 
 todoForm.addEventListener("submit", todoSubmit);
