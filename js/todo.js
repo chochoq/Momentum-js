f4a37c10cd8a294c875ddde287f2099a8b256129
chochoq <aubepe@gmail.com>
2021-08-29 19:38:35 +0900

    (add) todoList

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+function paintTodo(newTodo) {
+    const li =document.createElement("li");
+    const span = document.createElement("span");
+
+    li.appendChild(span);
+    span.innerHTML = newTodo;
+    todoList.appendChild(li)
+}
+
 function todoSubmit(e) {
     e.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
-
+    paintTodo(newTodo);
 }
 
 todoForm.addEventListener("submit", todoSubmit);
