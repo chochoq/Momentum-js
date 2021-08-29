afae1f0c59f22688c7a8b9f296f15ecf2604e19b
chochoq <aubepe@gmail.com>
2021-08-29 20:19:32 +0900

    (add) save todolist

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+const todos = [];
+
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
 function deleteTodoList(e) {
     const li = e.target.parentElement;
     li.remove();
@@ -18,14 +24,16 @@ function paintTodo(newTodo) {
 
     li.appendChild(span);
     li.appendChild(btn);
-    todoList.appendChild(li)
+    todoList.appendChild(li);
 };
 
 function todoSubmit(e) {
     e.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
+    todos.push(newTodo);
     paintTodo(newTodo);
+    saveTodos();
 };
 
 todoForm.addEventListener("submit", todoSubmit);
